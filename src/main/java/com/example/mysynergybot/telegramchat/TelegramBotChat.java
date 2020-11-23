d22a90f8059ea2a674df029942aec3b23caa6b41
Alekc2110 <0504268686@ukr.net>
2020-11-23 03:01:45 +0200

    deleted unnecessary methods

--- src/main/java/com/example/mysynergybot/telegramchat/TelegramBotChat.java
+++ src/main/java/com/example/mysynergybot/telegramchat/TelegramBotChat.java
@@ -1,7 +1,6 @@
 package  com.example.mysynergybot.telegramchat;
 
 import com.example.mysynergybot.telegramchat.entity.dto.ChatUserDto;
-import com.example.mysynergybot.telegramchat.entity.dto.UserDto;
 import com.example.mysynergybot.telegramchat.service.chatuserservice.ChatUserService;
 import com.example.mysynergybot.telegramchat.service.chatuserservice.TelegramBotApiService;
 import com.example.mysynergybot.telegramchat.service.chatuserservice.UserService;
@@ -10,17 +9,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.methods.send.SendContact;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Contact;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 import javax.annotation.PostConstruct;
-import java.util.*;
+import java.util.Objects;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 
@@ -69,25 +64,6 @@ public class TelegramBotChat extends TelegramLongPollingBot {
             botConnect();
         }
     }
-    @PostConstruct
-    public void getUserTelegramId(){
-//        List<UserDto> collected = userService.getUsers().stream()
-//               .filter(userDto -> userDto.getTelegramChatId() == null).collect(Collectors.toList());
-//        for (UserDto u : collected) {
-//            String userIdFromTelegram = userService.getUserIdFromTelegram(Math.round(Math.random()), u.getPhone());
-//            u.setTelegramChatId(Long.parseLong(userIdFromTelegram));
-//            userService.updateUser(u);
-    }
-
-
-//        List<UserDto> collected = userService.getUsers().stream()
-//                .filter(userDto -> userDto.getTelegramChatId() == null).collect(Collectors.toList());
-//        for (UserDto u : collected) {
-//            String userIdFromTelegram = userService.getUserIdFromTelegram(Math.round(Math.random()), u.getPhone());
-//            u.setTelegramChatId(Long.parseLong(userIdFromTelegram));
-//            userService.updateUser(u);
-//        }
-
 
 
     @Override
@@ -107,7 +83,6 @@ public class TelegramBotChat extends TelegramLongPollingBot {
           chatService.addChatUser(newUser);
 
        }
-
        receiveQueue.add(update);
 
        log.info("update - {} message.chat -{} message.contact -{} message.getFrom - {} getText - {}",
@@ -116,56 +91,6 @@ public class TelegramBotChat extends TelegramLongPollingBot {
                 update.getMessage().getText());
 
        }
-
-    private void replyToUser(SendMessage message) {
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            log.error("error {} when trying to reply to user", e.getMessage());
-        }
-    }
-
-
-
-//
-//    // несколько проверок, чтобы можно было отправлять сообщения другим пользователям
-//    private boolean canSendMessage(User user, Message msg) {
-//
-//        SendMessage answer = new SendMessage();
-//        answer.setChatId(msg.getChatId());
-//
-//        if (!msg.hasText() || msg.getText().trim().length() == 0) {
-//            log.info("User {} is trying to send empty message!", user.getId());
-//            answer.setText("You shouldn't send empty messages!");
-//            replyToUser(answer);
-//            return false;
-//        }
-
-//        if(!chatService.hasChatUser(user)) {
-//            log.info("User {} is trying to send message without starting the bot!", user.getId());
-//            answer.setText("Firstly you should start bot! Use /start command!");
-//            replyToUser(answer);
-//            return false;
-//        }
-
-//        if (chatService.getDisplayedName(user) == null) {
-//            log.info("User {} is trying to send message without setting a name!", user.getId());
-//            answer.setText("You must set a name before sending messages.\nUse '/set_name <displayed_name>' command.");
-//            replyToUser(answer);
-//            return false;
-//        }
-
-//        return true;
-//    }
-
-//    private void sendMessageToUsers(SendMessage message) {
-//        try {
-//            execute(message);
-//            } catch (TelegramApiException e) {
-//            log.error("error {} when trying to send massage to Users", e.getMessage());
-//        }
-//    }
-//
 
 
 
